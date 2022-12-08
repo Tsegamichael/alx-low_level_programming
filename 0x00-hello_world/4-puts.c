e755ffdd1c33febe3abc4af6fbec30f75a9109b3
tsegamichael <Tsegamichaelemail Tsegamichaelm@gmail.comclearexitgit config --global user.email Tsegamichaelemail>
2022-12-08 12:32:02 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+ * main - main block
+ * return: 0
+ * /
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+	return (0);

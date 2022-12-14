74adc42e89b267ea92c0153851471541814a0d2e
tsegamichael <Tsegamichaelemail Tsegamichaelm@gmail.comclearexitgit config --global user.email Tsegamichaelemail>
2022-12-14 00:50:00 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs -> computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
